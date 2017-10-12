b22250129aaaafcb9f69f329b3ce6d79b312b087
pranuthip <30725613+pranuthip@users.noreply.github.com>
2017-10-12 16:02:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req,res){
+    res.send('Article one is serving');
+});
+
+ 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

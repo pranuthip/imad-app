9f23a1072e950d684ae78d29692830c11ba07489
pranuthip <30725613+pranuthip@users.noreply.github.com>
2017-10-14 11:21:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,21 +86,10 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req,res){
     var articleName = req.params.articleName ;
-    res.send(createTemplate(articles[articleOne]));
+    res.send(createTemplate(articles[articleName]));
 });
 
 
-app.get('/article-four', function(req,res){
-    res.send('Article four is serving ');
-});
-
-app.get('/article-two', function(req,res){
-    res.send('Article two is serving');
-});
-
-app.get('/article-three', function(req,res){
-    res.send('Article three is serving');
-});
  
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

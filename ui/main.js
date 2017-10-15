b209814a1c3063124a1a10c3956a50aca71c39fc
pranuthip <30725613+pranuthip@users.noreply.github.com>
2017-10-15 12:10:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,5 @@
 console.log('Loaded!');
 
-//To change the text
-var element = document.getElementById('main-text');
-element.innerHTML = "My new html content in main js file";
-
-// To move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight ()  {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-  var interval = setInterval(moveRight,50);  
-};
 
 var button = document.getElementById('counter');
 var counter = 0;

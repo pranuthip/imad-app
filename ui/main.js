bf0fe395ac99e97c78d232950bc9eac2dfcd95b1
pranuthip <30725613+pranuthip@users.noreply.github.com>
2017-10-16 09:38:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,37 +1,8 @@
-//counter code 
+//Action to the button
 var button = document.getElementById('counter');
- button.onclick = function(){
-  //make a request to the counterendpoint
-  var request = new XMLHttpRequest();
-  
-  //capture the response & store it in a variable
-  request.onreadystatechange = function(){
-   if (request.readyState === XMLHttpRequest.Done){
-       //take some action
-       if (request.status === 200){
-           var counter = request.responseText;
-           var span = document.getElementById('count');
-           span.innerHTML = counter.toString();
-       }
-   }
-//not yet done
-   };
-   request.open('GET','http://pranuthitallapally.imad.hasura.io/counter',true);
-   request.send(null);
-};
-
-//submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-
-submit.onclick = function() {
-  //make a request to the server and send the name
-  //capture a list of names and render it as a list 
-  var names = ['name1','name2','name3','name4'];
-  var list = '';
-  for (var i=0, i < names.length; i++){
-      list +- '<li>' +names[i]+ '</li>';
-  }
-  var ul = document.getElementById('namelist');
+var counter = 0;
+button.onclick = function() {
+  counter = counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };

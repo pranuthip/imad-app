4159e5c45f53236ec168735e2afcb3f16926664a
pranuthip <30725613+pranuthip@users.noreply.github.com>
2017-10-16 18:44:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,18 +22,36 @@ request.onreadystatechange = function() {
 request.open('GET','http://pranuthitallapally.imad.hasura-app.io/counter', true);
 request.send(null);
 };
+
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //make a request to the server and send the name 
+    //create  a request  object to the counter endpoint
+        var request = new XMLHttpRequest();
+
+     //capture the response and store it in a variable
+        request.onreadystatechange = function() {
+           if (request.readyState === XMLHttpRequest.DONE){
+      //take some action
+      if (request.status === 200){
+          //make a request to the server and send the name 
     //capture a list of names ans render it as a list 
-    var names = ['name1','name2','name3','name4'];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for (var i=0;i<names.length; i++){
         list += '<li>' + names[i] + '</li>';
-    }
+          
+      }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+      }
+           }
+};
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+//make a request
+request.open('GET','http://pranuthitallapally.imad.hasura-app.io/submit-name?name=' +name, true);
+request.send(null);
 };
